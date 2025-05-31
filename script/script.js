edd70c18fbda3843e4f6d0acad22c3d6b7453a4a
JC Lefevre <jc.lefevre.job@gmail.com>
2025-05-31 19:13:42 +0200

    opti5

--- script/script.js
+++ script/script.js
@@ -105,7 +105,7 @@ function createSkillsFromJSON() {
                     <div class="card skillsText">
                         <div class="card-body">
                             <img src="./images/${item.image}" alt="logo de ${item.title}"/>
-                            <h4 class="card-title mt-3">${item.title}</h4>
+                            <h3 class="card-title mt-3">${item.title}</h3>
                             <p class="card-text mt-3">${item.text}</p>
                         </div>
                     </div>
@@ -146,7 +146,7 @@ function createPortfolioFromJSON() {
                     <div class="card portfolioContent">
                     <img class="card-img card-img-top" src="images/${item.image}" alt="image du projet ${item.title} " style="width:100%">
                     <div class="card-body">
-                        <h4 class="card-title">${item.title}</h4>
+                        <h3 class="card-title">${item.title}</h3>
                         <p class="card-text card-description">${item.text}</p>
                         <ul class= " custom-list mt-3">${missionsList}</ul>
                          
